5fbd47a2c54cda3302ad7f6717d99e4c5e05f532
0neday <guanyin7@icloud.com>
2021-07-30 07:58:45 +0800

    fix sshd, listen 2222, because lxc contianer do not kill dropbear

--- mnt/jffs2/hw/bin/start-sshd.sh
+++ mnt/jffs2/hw/bin/start-sshd.sh
@@ -5,18 +5,16 @@
 # sshd 
 if [ ! -e /tmp/sshd.log ]; then
 
-	cp -p /mnt/jffs2/hw/etc/authorized_keys /var/authorized_keys
-	chmod 600 /var/authorized_keys
 	mkdir -p /var/run
-
 	mkdir -p /var/empty
 	chmod 600 /var/empty
-	adduser -D -H sshd       
 
-	kill `pidof dropbear`
+	/mnt/jffs2/plug/app/bin/adduser -D -H sshd       
+
+#	kill `pidof dropbear`
 
 	/mnt/jffs2/hw/bin/sshd -f /mnt/jffs2/hw/etc/ssh/sshd_config -E /tmp/sshd.log 
-	/mnt/jffs2/plug/app/bin/iptables -I INPUT -s 192.168.1.1/24 -p tcp --dport 22 -j ACCEPT
+	/mnt/jffs2/plug/app/bin/iptables -I INPUT -s 192.168.1.1/24 -p tcp --dport 2222 -j ACCEPT
 fi
 
 exit 0;
